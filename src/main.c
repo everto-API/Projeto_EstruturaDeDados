ce1f57432e201d0ac9aba84abab96e86bed906c6
LuizManoelR <luiz.meneses@dcomp.ufs.br>
2026-02-12 18:02:35 -0300

    Modificando algumas estruturas

--- src/main.c
+++ src/main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
-struct Dados {
-    int ID;
+typedef struct Paciente {
     char Nome[50];
+    char cpf[11];
     int Idade;
-    int Prioridade;
-    char Diagnostico[100];
-    struct Dados *Proximo;}; 
-typedef struct Dados Dados;
-Dados ficha; // Variável principal ficha com o tipo Dados
+}Paciente;
+
+typedef struct Medico {
+    char Nome[50];
+    char crm[6];
+    char especialidade[30];
+}Medico;
+
+typedef struct Consulta {
+    Paciente paciente;
+    char especialidade[30];
+    char preferencial;
+}Consulta;
+typedef struct Emergencia {
+    Paciente paciente;
+    int prioridade;
+}Emergencia;
 
 void Incluir() {}; // Cadastrar o Paciente
 void Atendimento() {}; // Acho que pro médico dizer pro Paciente oq ele tem?
