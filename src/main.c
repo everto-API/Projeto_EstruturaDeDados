50d375a8e9a80083ab85510a58e3625b151660ab
Otavio071 <177013ot@academico.ufs.br>
2026-02-14 15:13:50 -0300

    implementação das funções incluir e buscarpaciente
    incluir eu usei  realloc pois a lista é dinâmica e precisa de ponteiros, a buscarpaciente usa uma busca linear com o for e o strcmp

--- src/main.c
+++ src/main.c
@@ -21,11 +21,53 @@ typedef struct Emergencia {
     Paciente paciente;
     int prioridade;
 }Emergencia;
+Paciente *listaPacientes = NULL;
+int quantidade = 0;
+void Incluir() {
+   
+    quantidade++; // aumenta a quantidade
+    listaPacientes = realloc(listaPacientes, quantidade * sizeof(Paciente));
+    if (listaPacientes == NULL) {
+        printf("Erro de alocação de memória!\n");
+        exit(1);
+    }
+     printf("Nome: ");
+    scanf(" %[^\n]", listaPacientes[quantidade - 1].Nome);
 
-void Incluir() {}; // Cadastrar o Paciente
+    printf("CPF: ");
+    scanf(" %s", listaPacientes[quantidade - 1].cpf);
+
+    printf("Idade: ");
+    scanf("%d", &listaPacientes[quantidade - 1].Idade);
+
+}; // Cadastrar o Paciente
 void Atendimento() {}; // Acho que pro médico dizer pro Paciente oq ele tem?
 
-void BuscarPaciente() {}; // Buscar um paciente em específico pelo ID;
+void BuscarPaciente(){
+     if (quantidade == 0) {
+        printf("Nenhum paciente cadastrado.\n");
+        return;
+    }
+
+    char cpfBusca[11];
+
+    printf("Digite o CPF para buscar: ");
+    scanf("%s", cpfBusca);
+
+    for (int i = 0; i < quantidade; i++) {
+
+        if (strcmp(listaPacientes[i].cpf, cpfBusca) == 0) {
+
+            printf("\nPaciente encontrado!\n");
+            printf("Nome: %s\n", listaPacientes[i].Nome);
+            printf("CPF: %s\n", listaPacientes[i].cpf);
+            printf("Idade: %d\n", listaPacientes[i].Idade);
+
+            return;
+        }
+    }
+}
+ ; // Buscar um paciente em específico pelo CPF;
 void ListarTodos() {}; // Listar todos os Pacientes cadastrados
 void LimparMemoria() {}; // Sujou, a polícia descobriu, apaga tudo!
 int main() {}; // a porra toda rola aqui B)
